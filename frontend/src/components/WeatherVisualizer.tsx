683ac8d8a72884be2a65eef0e5cbc4fe3a31e0b7
Youngsang-Cho1 <youngsang1211@gmail.com>
2026-04-10 21:58:44 -0400

    resolved issue regarding deployment

--- frontend/src/components/WeatherVisualizer.tsx
+++ frontend/src/components/WeatherVisualizer.tsx
@@ -38,7 +38,7 @@ interface WeatherSummary {
     chill: WeatherMetric[];
 }
 
-const API = "http://127.0.0.1:8000";
+const API = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 const LIVE_POLL_MS = 60 * 60 * 1000; // 1 hour
 
 interface DateRange {
